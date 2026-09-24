27ec510f07c2276cfbdf53176ff67d5354bf1e65
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-24 11:25:58 +0700

    feat(adapter): add sop-ta procedure compatibility boundary

---
+++ packages/adapter-sop-ta/src/index.ts
@@ -0,0 +1,14 @@
+export {
+  importSopTaDocument,
+  exportSopTaDocument,
+  validateSopTaCompatibility,
+  type SopTaCompatibilityIssue,
+} from "./procedureAdapter.js";
+export type {
+  SopTaActor,
+  SopTaDurationUnit,
+  SopTaImportInput,
+  SopTaProcedureRow,
+  SopTaStepType,
+  SopTaTerminatorRole,
+} from "./types.js";
